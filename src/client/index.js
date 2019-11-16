4a7a04e7a4b2f71b06849824585342f9f1d75362
tamasdinh <tamas.dinh@gmail.com>
2019-11-16 20:38:20 +0100

    refactor: factored destination submit into separate file
    also, now results retrieval works correctly as results variable is scoped higher

--- src/client/index.js
+++ src/client/index.js
@@ -2,7 +2,8 @@ import './styles/base.scss'
 import './js/app'
 import { loadTripData } from './js/loadTripData'
 import { populateSelectors } from './js/dateSelectors'
-import { destinationSelect } from './js/destinationSelect'
+import { destinationSelect, destinationSubmit } from './js/destinationSelect'
+import { durationSelector, durationSubmit } from './js/durationSelector'
 
 const now = new Date()
 let tripID
@@ -10,33 +11,19 @@ let tripID
 window.addEventListener('load', () => {
   tripID = loadTripData(now)
   populateSelectors(now, tripID)
+  durationSelector(tripID)
 })
 
 document.getElementById('destination-selector').addEventListener('input', (event) => destinationSelect(event))
 
-document.getElementById('destination-submit').addEventListener('click', () => {
-  const trip = JSON.parse(localStorage.getItem(tripID))
-  trip.destination = undefined
-  const selection = document.getElementById('destination-selector').value
-  trip.destination = Object.values(JSON.parse(localStorage.getItem('results'))).filter(item => 
-    item.name == selection.split(', ')[0] &&
-    item.adminCode1 == selection.split(', ')[1] &&
-    item.countryName == selection.split(', ')[2]
-  )[0]
-  if (!trip.destination) {
-    alert('Please select location from drop-down list and click [Submit]!')
-  }
-  localStorage.setItem(tripID, JSON.stringify(trip))
-  console.log(trip.destination)
+document.getElementById('destination-submit').addEventListener('click', () => destinationSubmit(tripID))
 
-  const destinationData = JSON.parse(localStorage.getItem('results'))
-  console.log(destinationData)
+document.getElementById('duration-selector').addEventListener('change', (event) => durationSubmit(event, tripID))
 
-})
-
-
+/*
 document.getElementById('remove-trip').addEventListener('click', () => {
   localStorage.removeItem(tripID)
   tripID = null
   console.log(localStorage)
 })
+*/
--- src/client/js/destinationSelect.js
+++ src/client/js/destinationSelect.js
@@ -13,11 +13,12 @@ export function destinationSelect (event) {
     let params = {query: event.target.value}
     Object.keys(params).forEach(key => urlToUse.searchParams.append(key, params[key]))
     
+    let results = []
+
     fetch(urlToUse)
     .then(res => res.json())
     .then(res => {
       console.log(res)
-      let results = []
       res.geonames.forEach(item => {
         results.push({
           name: item.name,
@@ -32,7 +33,7 @@ export function destinationSelect (event) {
       
       let resultsToReturn = Math.min(results.length, 20)
       const destOptions = document.createDocumentFragment()
-      for (let i = 0; i <= resultsToReturn; i++) {
+      for (let i = 0; i < resultsToReturn; i++) {
         const item = document.createElement('option')
         item.value = `${results[i].name}, ${results[i].adminCode1}, ${results[i].countryName}`
         destOptions.appendChild(item)
@@ -40,6 +41,24 @@ export function destinationSelect (event) {
       document.getElementById('destination-options').appendChild(destOptions)
 
     })
-    .catch(e => alert('An error has occurred during fetching. Please try again later.', e))
+    .catch(e => {
+      alert('An error has occurred during fetching. Please try again later.')
+      console.log(e) 
+    })
   }
+}
+
+export function destinationSubmit (tripID) {
+  const trip = JSON.parse(localStorage.getItem(tripID))
+  trip.destination = undefined
+  const selection = document.getElementById('destination-selector').value
+  trip.destination = Object.values(JSON.parse(localStorage.getItem('results'))).filter(item => 
+    item.name == selection.split(', ')[0] &&
+    item.adminCode1 == selection.split(', ')[1] &&
+    item.countryName == selection.split(', ')[2]
+  )[0]
+  if (!trip.destination) {
+    alert('Please select location from drop-down list and click [Submit]!')
+  }
+  localStorage.setItem(tripID, JSON.stringify(trip))
 }

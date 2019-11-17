a4e0c1d13f2d40e237be8cddda6e448242ab5326
tamasdinh <tamas.dinh@gmail.com>
2019-11-17 17:54:44 +0100

    fix: fixed destination selection

--- src/client/index.js
+++ src/client/index.js
@@ -1,24 +1,79 @@
+
+// TODO!!! check how we can get all results from geoames API (not just 100)
+
+
 import './styles/base.scss'
 import './js/app'
 import { loadTripData } from './js/loadTripData'
 import { populateSelectors } from './js/dateSelectors'
-import { destinationSelect, destinationSubmit } from './js/destinationSelect'
+import { getDestinations, tearDownDataList, serveDestinationOptions, destinationSubmit } from './js/destinationSelect'
 import { durationSelector, durationSubmit } from './js/durationSelector'
 
 const now = new Date()
 let tripID
+let prevLength = 0
+let destResults = []
 
 window.addEventListener('load', () => {
   tripID = loadTripData(now)
   populateSelectors(now, tripID)
   durationSelector(tripID)
+
+  const trip = JSON.parse(localStorage.getItem(tripID))
+  if (trip.destination) {
+    document.getElementById('destination-selector').value =
+    `${trip.destination.name}, ${trip.destination.countryName}, ${trip.destination.adminCode1}`
+  }
 })
 
-document.getElementById('destination-selector').addEventListener('input', (event) => destinationSelect(event))
+document.getElementById('destination-selector').addEventListener('input', (event) => {
+  if (event.target.value.length == 4 && prevLength < event.target.value.length) {
+    getDestinations(event)
+    .then(results => {
+      destResults = results
+      return destResults
+    })
+    .then(destResults => {
+      tearDownDataList()
+      serveDestinationOptions(event, destResults)
+    })
+  } else if (destResults.length > 0) {
+    tearDownDataList()
+    serveDestinationOptions(event, destResults)
+  }
+  prevLength = event.target.value.length
+  if (event.target.value.length < 4) {
+    console.log('Clearing destResults')
+    destResults = []
+    tearDownDataList()
+  }
+})
 
-document.getElementById('destination-submit').addEventListener('click', () => destinationSubmit(tripID))
+document.getElementById('destination-submit').addEventListener('click', () => {
+  destinationSubmit(tripID, destResults).then(console.log('Trip data saved.'))
+})
 
 document.getElementById('duration-selector').addEventListener('change', (event) => durationSubmit(event, tripID))
+
+document.getElementById('remove-destination-text').addEventListener('click', () => {
+  document.getElementById('destination-selector').value = ''
+  
+  for (let htmlContainer of
+    [document.getElementById('destination-options'),
+    document.getElementById('hero-shot'),
+    document.getElementById('photogrid')]) {
+
+      while (htmlContainer.firstChild) {
+        htmlContainer.removeChild(htmlContainer.firstChild)
+      }
+  }
+
+  const trip = JSON.parse(localStorage.getItem(tripID))
+  delete trip.destination
+  delete trip.images
+  localStorage.setItem(tripID, JSON.stringify(trip))
+})
+
 
 /*
 document.getElementById('remove-trip').addEventListener('click', () => {
--- src/client/js/destinationSelect.js
+++ src/client/js/destinationSelect.js
@@ -1,24 +1,25 @@
-export function destinationSelect (event) {
+import { getPhotos } from './getPhotos'
+
+export function getDestinations (event) {
 
   const serverURL = 'http://localhost'
   const port = 8081
   const placeEndPoint = 'places'
 
   let urlToUse = new URL(`${serverURL}:${port}/${placeEndPoint}`)
-  const optDropDown = document.getElementById('destination-options')
-  if (!event.target.value.includes(',') && event.target.value.length >= 4) {
-    while (optDropDown.firstChild) {
-      optDropDown.removeChild(optDropDown.firstChild)
-    }
-    let params = {query: event.target.value}
-    Object.keys(params).forEach(key => urlToUse.searchParams.append(key, params[key]))
-    
-    let results = []
-
+  const query = event.target.value.split(', ')
+  let nameStartsWith = query[0].match(/[a-zA-Z]*/)[0]
+ 
+  let params = {
+    nameStartsWith,
+  }
+  Object.keys(params).forEach(key => urlToUse.searchParams.append(key, params[key]))
+  
+  let results = []
+  return new Promise((resolve, reject) => {
     fetch(urlToUse)
     .then(res => res.json())
     .then(res => {
-      console.log(res)
       res.geonames.forEach(item => {
         results.push({
           name: item.name,
@@ -29,36 +30,79 @@ export function destinationSelect (event) {
           lng: item.lng
         })
       })
-      localStorage.setItem('results', JSON.stringify(results))
-      
-      let resultsToReturn = Math.min(results.length, 20)
-      const destOptions = document.createDocumentFragment()
-      for (let i = 0; i < resultsToReturn; i++) {
-        const item = document.createElement('option')
-        item.value = `${results[i].name}, ${results[i].adminCode1}, ${results[i].countryName}`
-        destOptions.appendChild(item)
-      }
-      document.getElementById('destination-options').appendChild(destOptions)
-
+      return results
     })
+    .then(results => resolve(results))
     .catch(e => {
       alert('An error has occurred during fetching. Please try again later.')
-      console.log(e) 
+      reject(e) 
     })
+  })
+}
+
+export function tearDownDataList () {
+  const optDropDown = document.getElementById('destination-options')
+  console.log('Destroying datalist')
+  while (optDropDown.firstChild) {
+    optDropDown.removeChild(optDropDown.firstChild)
   }
 }
 
-export function destinationSubmit (tripID) {
-  const trip = JSON.parse(localStorage.getItem(tripID))
-  trip.destination = undefined
-  const selection = document.getElementById('destination-selector').value
-  trip.destination = Object.values(JSON.parse(localStorage.getItem('results'))).filter(item => 
-    item.name == selection.split(', ')[0] &&
-    item.adminCode1 == selection.split(', ')[1] &&
-    item.countryName == selection.split(', ')[2]
-  )[0]
-  if (!trip.destination) {
-    alert('Please select location from drop-down list and click [Submit]!')
-  }
-  localStorage.setItem(tripID, JSON.stringify(trip))
+export function serveDestinationOptions (event, results) {
+  console.log('datalist received this many results:', results.length)
+  
+  const relevantResults = results.filter(resultItem => {
+    const queryItems = event.target.value.split(' ').map(item => (
+      item.match(/[a-zA-Z]*/)[0]
+      ))
+      let inclusionCounter = 0
+      for (let item of queryItems) {
+        if ((resultItem.name + resultItem.countryName).includes(item)) {
+          inclusionCounter += 1
+        }
+      }
+      return inclusionCounter == queryItems.length
+  })
+  console.log(relevantResults)
+
+  const optDropDown = document.getElementById('destination-options')
+  
+  console.log('Building datalist; input length:', event.target.value.length)
+  let resultsToReturn = Math.min(relevantResults.length, 10)
+  const destOptions = document.createDocumentFragment()
+  for (let i = 0; i < resultsToReturn; i++) {
+    const item = document.createElement('option')
+    item.value = `${relevantResults[i].name}, ${relevantResults[i].countryName}, ${relevantResults[i].adminCode1}`
+    const isOnlyOne = event.target.value == item.value || relevantResults.length == 1
+    if (!isOnlyOne) {
+      destOptions.appendChild(item)
+    }
+  } 
+  optDropDown.appendChild(destOptions)
+}
+
+export function destinationSubmit (tripID, destinationResults) {
+  const selectionSplit = document.getElementById('destination-selector').value.split(', ')
+  const destinationQuery = `${selectionSplit[0]}, ${selectionSplit[1]}`
+  let images = []
+  return new Promise((resolve, reject) => {
+    getPhotos(tripID, destinationQuery, images)
+    .then((images) => {
+      const trip = JSON.parse(localStorage.getItem(tripID))
+      trip.destination = destinationResults.filter(item => (
+          item.adminCode1 == selectionSplit[2] &&
+          item.name == selectionSplit[0] &&
+          item.countryName == selectionSplit[1]
+      ))[0]
+      trip.images = images
+      console.log('Trip data saved:', trip)
+      localStorage.setItem(tripID, JSON.stringify(trip))
+      localStorage.removeItem('results')
+      resolve()
+    })
+    .catch(e => {
+      alert('An error has occurred during execution.')
+      reject(e)
+    })
+  })
 }
